4d59e893cce2ebe43cc42a5c4ab53a1427d8d75b
xiaozhong9 <250353831@qq.com>
2024-01-06 14:33:04 +0800

    数列排序

--- test.py
+++ test.py
@@ -1,2 +1,16 @@
-print("hello,world")
-print("==========================123")
+#冒泡排序
+n=int(input())
+nums=list(map(int,input().split()))
+i=0
+while i < n - 1:
+    flag = True
+    j = 0
+    while j < n-i-1 :
+        if nums[j] > nums[j+1]:
+            nums[j], nums[j+1] = nums[j+1], nums[j]
+            flag = False
+        j += 1
+    if flag:
+        break
+    i += 1
+print(*nums)
